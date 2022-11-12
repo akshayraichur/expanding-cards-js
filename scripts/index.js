33f84cf3e906a2d123936983acc286d2b40f6de8
Akshay Raichur <akshay.raichur9@gmail.com>
2022-11-12 20:25:40 +0530

    fix: remove log statement

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@ const panels = document.querySelectorAll('.panel');
 const container = document.querySelector('.container');
 
 container.addEventListener('click', (e) => {
-  console.log(e.target);
   const currentSelectedPanel = document.querySelector('.active');
   currentSelectedPanel.classList.remove('active');
   e.target.classList.add('active');
